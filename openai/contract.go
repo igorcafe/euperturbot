d374d58da9c8d26722955a9b324375b35d6b3ef7
Igor Melo <igoracmelo@protonmail.com>
2024-04-25 19:42:36 -0300

    openai: separate contract from impl

---
+++ openai/contract.go
@@ -0,0 +1,28 @@
+package openai
+
+type Service interface {
+	Completion(params *CompletionParams) (*CompletionResponse, error)
+}
+
+type CompletionParams struct {
+	WaitRateLimit bool
+	Model         string
+	Messages      []Message
+	Temperature   float64
+}
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type CompletionResponse struct {
+	Choices []struct {
+		Message Message `json:"message"`
+	}
+}
+
+type ErrRateLimit int
+
+func (err ErrRateLimit) Error() string {
+	return "rate limit"
+}
--- openai/openai.go
+++ openai/openai.go
@@ -11,10 +11,6 @@ import (
 	"github.com/igoracmelo/euperturbot/util"
 )
 
-type Service interface {
-	Completion(params *CompletionParams) (*CompletionResponse, error)
-}
-
 type service struct {
 	key               string
 	http              *http.Client
@@ -31,29 +27,6 @@ func NewService(key string, http *http.Client) Service {
 		mut:               new(sync.Mutex),
 		rateLimitDeadline: deadline,
 	}
-}
-
-type CompletionParams struct {
-	WaitRateLimit bool
-	Model         string
-	Messages      []Message
-	Temperature   float64
-}
-type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type CompletionResponse struct {
-	Choices []struct {
-		Message Message `json:"message"`
-	}
-}
-
-type ErrRateLimit int
-
-func (err ErrRateLimit) Error() string {
-	return "rate limit"
 }
 
 func (s *service) Completion(params *CompletionParams) (*CompletionResponse, error) {
